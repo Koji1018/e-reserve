09729b7f06211129acf63d06ebfd9942450204c5
daiki.kojima5 <dkojima@collabo-sys.jp>
2021-05-25 02:47:41 +0000

    top page

---
+++ app/Http/Controllers/ReservationsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Reservation;
+
+class ReservationsController extends Controller
+{
+    // トップページ画面の表示用
+    public function index()
+    {
+        $data = [];
+		if (\Auth::check()) { // 認証済みの場合				
+			$user = \Auth::user(); // 認証済みユーザを取得
+			$reservations = $user->reservations()->orderBy('lending_start', 'asc')->paginate(10); // ユーザの予約一覧を貸出開始日時の昇順で取得
+			$data = [
+				'user' => $user,
+				'reservations' => $reservations,
+			];
+			return view('reservations.index_user', $data); // lendings/userビューでそれらを表示
+		} else return view('auth.login');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_05_24_105055_create_equipments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');    // nameカラム追加
+            $table->unsignedBigInteger('category_id'); // category_idカラム追加
+            $table->unsignedBigInteger('status');  // statusカラム追加
+            $table->timestamps();
+            
+            // 外部キー制約
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+}
