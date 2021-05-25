d0b97ecf2b33f7b74ef1200f2036aa2cc1d8338f
daiki.kojima5 <dkojima@collabo-sys.jp>
2021-05-25 04:13:05 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'user_group' => 1, // ユーザ種別コードの追加
         ]);
     }
 }
--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -14,10 +14,10 @@ class ReservationsController extends Controller
         $data = [];
 		if (\Auth::check()) { // 認証済みの場合				
 			$user = \Auth::user(); // 認証済みユーザを取得
-			$reservations = $user->reservations()->orderBy('lending_start', 'asc')->paginate(10); // ユーザの予約一覧を貸出開始日時の昇順で取得
+// 			$reservations = $user->reservations()->orderBy('lending_start', 'asc')->paginate(10); // ユーザの予約一覧を貸出開始日時の昇順で取得
 			$data = [
 				'user' => $user,
-				'reservations' => $reservations,
+				// 'reservations' => $reservations,
 			];
 			return view('reservations.index_user', $data); // lendings/userビューでそれらを表示
 		} else return view('auth.login');
