1054f1c60e8df853f41a4d3470e18d5bbff94e38
daiki.kojima5 <dkojima@collabo-sys.jp>
2021-06-15 10:15:01 +0000

    Seeder Create

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(Categories_Equipments_TableSeeder::class);
-        $this->call(UserTableSeeder::class);
+        //$this->call(Categories_Equipments_TableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
